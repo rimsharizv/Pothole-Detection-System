67201427cd6b032b5b0261d2dbe1198a483814d9
Rimsha Rizvi <78344946+rimsharizv@users.noreply.github.com>
2025-01-20 16:53:34 -0600

    Delete unnecessary files

--- Code/lib/firebase_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class FirebaseService {
-  final CollectionReference _potholesCollection = FirebaseFirestore.instance.collection('potholes');
-
-  Stream<QuerySnapshot> getPotholesStream() {
-    return _potholesCollection.snapshots();
-  }
-}
